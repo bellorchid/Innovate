597977d8ddcf598e757b92438554ddacc87424a3
bellorchid <zhongyunchang@yeah.net>
2015-06-01 00:39:16 +0800

    变量错误bug修改

--- resources/views/projects/table.blade.php
+++ resources/views/projects/table.blade.php
@@ -8,7 +8,7 @@
 <tr><th>项目名称</th><th>项目简介</th><th>GitHub地址</th><th>Demo地址</th><th>详细介绍</th></tr>
 @foreach($project as $projects )
 
-	<tr><th>{{$projects->name}}</th><th>{{$projects->abstract}}</th><th>{{$projects->github}}</th><th>{{$projects->demo}}</th><th>{{$projects->detail}}</th></tr>
+	<tr><th>{{$projects->name}}</th><th>{{$projects->abstract}}</th><th>{{$projects->address}}</th><th>{{$projects->demo}}</th><th>{{$projects->detail}}</th></tr>
 @endforeach
 </table>
 
